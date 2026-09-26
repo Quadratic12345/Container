c6a7c7050863c4da1aa2bdab0474c127cda5a913
sankalp-kar <programmercoding01234@gmail.com>
2026-09-26 14:47:20 +0530

    fix

--- src/namespace.c
+++ src/namespace.c
@@ -1,24 +1,34 @@
+#include <sched.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <unistd.h>
 
-#include<sched.h>
-#include<stdio.h>
-#include<string.h>
-#include<sys/mount.h>// as it is also mount namespace
-#include<unistd.h>
-#include "namespace.h" // the namespace header file is here
+#include "namespace.h"
 
-//now to setup the hostname
-int setup_hostname(const char *hostname){
-    if(sethostname(hostname,strlen(hostname))==-1){
+int setup_hostname(const char *hostname)
+{
+    if (sethostname(hostname, strlen(hostname)) == -1) {
         perror("sethostname");
         return -1;
     }
+
     return 0;
 }
-//now to make the mounts private for namespaces of the container and hostname
-int setup_mount_namespace(void){
-    if(mount(NULL,"/",NULL,MS_REC | MS_PRIVATE,NULL)==-1){
-        perror("mounting");
+
+int setup_mount_namespace(void)
+{
+    if (mount(
+            NULL,
+            "/",
+            NULL,
+            MS_REC | MS_PRIVATE,
+            NULL
+        ) == -1)
+    {
+        perror("mount MS_PRIVATE");
         return -1;
     }
+
     return 0;
 }
